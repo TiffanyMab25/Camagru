21ac4f546bc2d5e1c52ea4f1843855618dd7ee23
Tiffany <50674487+TiffanyMab25@users.noreply.github.com>
2019-10-28 18:59:59 +0200

    Add files via upload

---
+++ login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr"
+<head>
+<meta charset="utf-8">
+<title>Camagru</title>
+</head>
+<body>
+<div class="">
+<h3>Camagru<h3>
+<form action="login.php" method="post">
+ <input type="text" name="username-email value="" placeholder="username or email"><br>
+ <input type="text" name="passwd" value="" placeholder="Password"><br>
+<input type="submit" name="login-btn" value="Login"><br>
+ <p>Don't have an account? <a href="signup.php">Sign up</a></p>
+</form>
+</div>
+</body>
+</html>
