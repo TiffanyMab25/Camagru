a5a4a6717ff9d9bdbec327305338a83286aa7057
Tiffany <50674487+TiffanyMab25@users.noreply.github.com>
2019-10-28 18:59:39 +0200

    Add files via upload

---
+++ signup.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr"
+<head>
+<meta charset="utf-8">
+<title>Signup</title>
+</head>
+<body>
+<div class="">
+<h3>Camagru<h3>
+<form action="signup.php" method="post">
+ <input type="text" name="email" value="" placeholder="email"><br>
+<input type="text" name="username" value="" placedholder=""><br>
+<input type="text" name="" value="" placedholder=""><br>
+<input type="submit" name="login-btn" value="Signup"><br>
+   <label>
+    <input type="checkbox" checked="checked" name="remember" style="margin-bottom:15px"> Remember me
+    </label>
+ <p>Have an account? <a href="login.php">Login</a></p>
+</form>
+</div>
+</body>
+</html>
