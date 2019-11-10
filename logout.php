f100d46dc13ed92309e233cbef02029af8d01842
Tiffany Mabunda <tiffanymabunda@Tiffanys-MacBook-Air.local>
2019-11-10 19:04:47 +0200

    Sunday

--- logout.php
+++ logout.php
@@ -1,4 +1,12 @@
 <?php
-
+ if (isset($_GET['logout'])) {
+    session_destroy();
+    unset($_SESSION['id']);
+    unset($_SESSION['username']);
+    unset($_SESSION['email']);
+    unset($_SESSION['verified']);
+    header('location: login.php');
+    exit();
+  }
 
 ?>
